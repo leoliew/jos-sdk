208a3a4cfd0d1e1b084d379946c08ad7ba6b746b
Leo <zmliu0077@gmail.com>
2017-12-15 11:48:03 +0800

    add api

--- test/jd_client.test.js
+++ test/jd_client.test.js
@@ -6,11 +6,21 @@ const JDClient = require('../lib/index').JDClient;
 const jd_config = require('./jd_config');
 
 describe('JDClient:handleAPI Test', () => {
-  const params = ['1090817274', '11024717589'];
-  const client = new JDClient(jd_config);
-  it('Should be tested', async () => {
+
+  it('Should getProductInfo success!', async () => {
+    const params = ['1090817274', '11024717589'];
+    const client = new JDClient(jd_config);
     const results = await client.getProductInfo(params);
     results[0].skuId.should.be.equal(1090817274);
     results[1].skuId.should.be.equal(11024717589);
+  });
+  it('Should batchGetCode success!', async () => {
+    const params = {
+      ids: ['1090817274', '11024717589']
+    }
+    const client = new JDClient(jd_config);
+    const results = await client.batchGetCode(params);
+    results[0].id.should.be.equal(1090817274);
+    results[1].id.should.be.equal(11024717589);
   });
 });
