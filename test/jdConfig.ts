de3eed6a2e1f69207edf8ed3b6e64a53961a7acb
Leo Liu <zmliu0077@gmail.com>
2017-12-18 01:28:05 +0800

    delete file

--- test/jdConfig.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 测试资源配置 (拷贝文件到当前目录下, 重命名为jd_config.js )
- * @type {{access_token: string, app_secret: string, app_key: string, format: string, v: string}}
- */
-module.exports = {
-  accessToken: '18967f16-c032-45d5-8c10-fa390379f6e5',
-  appKey: '2A9BA36996ACB843E2180966D9599191',
-  appSecret: '1ca97672674d4c118148c12db3abb900'
-}
